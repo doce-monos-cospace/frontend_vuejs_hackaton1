a4d10c7a2933637149b0cd63caf88bc4944ee0db
raskardev <alexcdev95@gmail.com>
2019-09-28 20:10:56 +0200

    feat(): create Header's child components

---
+++ src/components/Header/components/TaskNameInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="timer-content-left w-100">
+    <div class="d-flex">
+      <p id="task-name" @keyup="changeTaskNameInput()" class="w-100" contenteditable="true">
+        Nombre de la tarea
+      </p>
+      <div class="date button-timer flex-shrink-1">0:00:00</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskNameInput',
+};
+</script>
+
+<style></style>
---
+++ src/components/Header/components/TimerButtonTypes.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="timer-button-type">
+    <TimerButtonNormal />
+    <TimerButtonCountdown />
+  </div>
+</template>
+
+<script>
+import TimerButtonNormal from './TimerButtonNormal.vue';
+import TimerButtonCountdown from './TimerButtonCountdown.vue';
+
+export default {
+  name: 'TimerButtonTypes',
+  components: {
+    TimerButtonNormal,
+    TimerButtonCountdown,
+  },
+};
+</script>
+
+<style></style>
